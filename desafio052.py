5d2c6d855f0175b3c361dd476ad734b896b60423
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2025-01-22 17:42:39 -0300

    Add files via upload

--- desafio052.py
+++ desafio052.py
@@ -1,16 +1,18 @@
 num = int(input("Informe um número: "))
-if num % 2 == 0 or num % 3 == 0:
-    print(f"{num} não é um número primo.")
-elif num <= 1:
+if num <= 1:
     print(f"{num} não é um número primo.")
 elif num <= 3:
-    print(f"{num} não é um número primo.")
+     print(f"{num} é um número primo.")
+elif num % 2 == 0 or num % 3 == 0:
+    print(f"{num} Não é um numero primo.")
 else:
-    for c in range(num - 1, 1, -1):
+    primo = True
+    for c in range(5, int(num ** 0.5) + 1, 2):
         if num % c == 0:
-            
-        print(f"O número {num} não é primo.")
-        exit()
-        else:
+            primo = False
+            print(f"O número {num} não é primo.")
+            break
+    if primo:
             print(f"O número {num} é primo.")
-            exit()
+    else:
+         print(f"O numero {num} não é primo.")
