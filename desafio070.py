4ec1b2996f3351fcda4b6ab7da8b3e76da455952
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2025-02-20 13:12:25 -0300

    Update desafio070.py

--- desafio070.py
+++ desafio070.py
@@ -3,29 +3,25 @@ cont = 0
 nome = ""
 maisBarato = ""
 menorValor = 0
+maisDeMil = 0
 print("-" * 30)
 print("LOJA SUPER BARATÃO")
 print("-" * 30)
-nome = str(input("Nome do produto: "))
-valor = int(input("Preço: R$"))
-menorValor = valor
-maisBarato = nome
-if valor >= 1000:
+while True:
+    nome = str(input("Nome do produto: "))
     cont += 1
-total += valor
-continuar = str(input("Deseja continuar? [S/N] ")).strip().upper()
-while continuar != "N":
-    while True:
-        nome = str(input("Nome do produto: "))
-        valor = int(input("Preço: R$"))
-        if valor < menorValor:
-            maisBarato = nome
-        if valor >= 1000:
-            cont += 1
-        total += valor
-        continuar = str(input("Deseja continuar? [S/N] ")).strip().upper()
-        if continuar == "N":
-            break
+    valor = int(input("Preço: R$"))
+    if cont == 1:
+        menorValor = valor
+        maisBarato = nome
+    if valor < menorValor:
+        maisBarato = nome
+    if valor >= 1000:
+        maisDeMil += 1
+    total += valor
+    continuar = str(input("Deseja continuar? [S/N] ")).strip().upper()
+    if continuar == "N":
+        break
 print(f"O produto de menor valor é {maisBarato}")
-print(f"Foram comprados {cont} produtos de mais de R$1000,00")
+print(f"Foram comprados {maisDeMil} produtos de mais de R$1000,00")
 print(f"O total da compra é R${total}")
